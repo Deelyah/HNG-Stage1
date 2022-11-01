4cef0578b94a253062b8130bbcd2d497297faa0e
PraiseDelia <elemdelia@gmail.com>
2022-11-01 13:07:21 +0100

    chore: add large screen view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 import profileImage from "../assets/profile-img.svg";
 import kebabMenu from "../assets/kebab-menu.svg";
+import backIcon from "../assets/back-icon.svg";
+import slackIcon from "../assets/slack.svg";
+import githubIcon from "../assets/github.svg";
 function Home() {
   const links = [
     {
@@ -12,7 +15,7 @@ function Home() {
     { name: "Pythin Books", id: "pitch", href: "https://books.zuri.team" },
     {
       name: "Background Check for Coders",
-      id: "",
+      id: "deelyah",
       href: "https://background.zuri.team",
     },
     {
@@ -22,8 +25,8 @@ function Home() {
     },
   ];
   return (
-    <div className="App">
-      <div className="flex flex-col py-16 px-4">
+    <div className="flex justify-center w-full">
+      <div className="flex flex-col py-16 px-4 w-full md:w-10/12">
         <div className="relative mb-8">
           <div className="w-full flex flex-col items-center">
             <div className="mb-6 rounded-full">
@@ -38,14 +41,15 @@ function Home() {
             {/* <h2 id="slack" >Deelyah</h2> */}
           </div>
           <button className="absolute right-0 -top-5 w-10 h-10 border border-dashed border-[#D0D5DD] flex justify-center items-center rounded-full">
-            <img src={kebabMenu} alt="menu" />
+            <img src={kebabMenu} alt="menu" className="md:hidden" />
+            <img src={backIcon} alt="back" className="hidden md:inline-block" />
           </button>
         </div>
         <div>
           {links.map((link, index) => {
             return (
               <a
-                className="block cursor-pointer text-center rounded-lg bg-[#EAECF0] text-[#101828] font-medium text-sm py-6 mb-6"
+                className="block cursor-pointer text-center rounded-lg bg-[#EAECF0] text-[#101828] font-medium text-sm md:text-lg py-6 mb-6"
                 href={link.href}
                 id={link.id}
                 key={index}
@@ -55,7 +59,10 @@ function Home() {
             );
           })}
         </div>
-        <div></div>
+        <div className="flex w-full justify-center py-6">
+          <img src={slackIcon} alt="slack icon" className="mr-6" />
+          <img src={githubIcon} alt="github icon" className="" />
+        </div>
       </div>
     </div>
   );
